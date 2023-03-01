a1db98f7ac637840953f5b101c79d3dd6199f306
Waris Works <warisruzi@gmail.com>
2023-03-01 16:55:31 +0900

    Part 2 Passing Namespace

--- Derisler/Views/MatchView.swift
+++ Derisler/Views/MatchView.swift
@@ -15,49 +15,119 @@ struct MatchView: View {
         ZStack {
             if !show {
                 VStack(alignment: .trailing, spacing: 12) {
-                    Text(" SwiftUI ھەققىدە")
-                        .font(.custom(mainfont, size: 30))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity,alignment: .trailing)
-                    Text("3سائەت 20 دەرس ")
-                        .font(.custom(mainfont, size: 16))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    
-                    Text("iOS 16 گە ئەپ قۇرۇش ۋە خاسلاشتۇرۇش")
-                        .font(.custom(mainfont, size: 16))
-                        .matchedGeometryEffect(id: "text", in: namespace)
-
+                    Spacer()
+                    VStack {
+                        Text(" SwiftUI ھەققىدە")
+                            .font(.custom(mainfont, size: 30))
+                            .matchedGeometryEffect(id: "title", in: namespace)
+                            .frame(maxWidth: .infinity,alignment: .trailing)
+                        Text("3سائەت 20 دەرس ")
+                            .font(.custom(mainfont, size: 16))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            .frame(maxWidth: .infinity,alignment: .trailing)
+                        
+                        Text("ئايفونغا  SwiftUI تىلىدا ئەپ ئېچىش ")
+                            .font(.custom(mainfont, size: 16))
+                            .matchedGeometryEffect(id: "text", in: namespace)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .blur(radius: 30)
+                            .matchedGeometryEffect(id: "blur", in: namespace)
+                    )
                 }
-                .padding(20)
                 .foregroundColor(.white)
                 .background(
-                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                    Image("Illustration 9")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "image", in: namespace)
                 )
+                .background(
+                    Image("Background 5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .mask (
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                )
+                .frame(height: 300)
                 .padding(20)
             }else {
-                VStack(alignment: .trailing, spacing: 12) {
-                    Spacer()
-                    Text(" SwiftUI ھەققىدە")
-                        .font(.custom(mainfont, size: 30))
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity,alignment: .trailing)
-                    Text("3سائەت 20 دەرس ")
-                        .font(.custom(mainfont, size: 16))
-                        .matchedGeometryEffect(id: "subtitle", in: namespace)
-                    
-                    Text("iOS 16 گە ئەپ قۇرۇش ۋە خاسلاشتۇرۇش")
-                        .font(.custom(mainfont, size: 16))
-                        .matchedGeometryEffect(id: "text", in: namespace)
-                        .frame(maxWidth: .infinity,alignment: .trailing)
-                    
-                }
-                .padding(20)
-                .foregroundColor(.black)
-                .background(
-                    Color.blue.matchedGeometryEffect(id: "background", in: namespace)
-                )
+                ScrollView {
+                    VStack{
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    // .padding(20)
+                    .foregroundColor(.black)
+                    .background(
+                        Image("Illustration 9")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("Background 5")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask (
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                    )
+                    .overlay(
+                        VStack(alignment: .trailing, spacing: 12) {
+                            
+                            Text(" SwiftUI ھەققىدە")
+                                .font(.custom(mainfont, size: 30))
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                                .frame(maxWidth: .infinity,alignment: .trailing)
+                            
+                            Text("3سائەت 20 دەرس ")
+                                .font(.custom(mainfont, size: 16))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+                            
+                            Text("ئايفونغا  SwiftUI تىلىدا ئەپ ئېچىش ")
+                                .font(.custom(mainfont, size: 16))
+                                .matchedGeometryEffect(id: "text", in: namespace)
+                                .frame(maxWidth: .infinity,alignment: .trailing)
 
-                
+                            Divider()
+                            HStack{
+                                Text("دەرس سۆزلىگۈچى : ئوغۇز")
+                                    .font(.custom(mainfont, size: 14))
+                                
+                                Image("Avatar Default")
+                                    .resizable()
+                                    .frame(width: 26, height: 26)
+                                    .cornerRadius(10)
+                                    .padding(8)
+                                    .background(.ultraThinMaterial, in:  RoundedRectangle(cornerRadius: 18, style: .continuous))
+                                    .strokeStyle(cornerRadius: 18)
+                                
+                            }
+                            
+                        }
+                            .padding(20)
+                            .background(
+                                Rectangle()
+                                    .fill(.ultraThinMaterial)
+                                    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                    .matchedGeometryEffect(id: "blur", in: namespace)
+                                
+                            )
+                            .offset(y: 250)
+                            .padding(20)
+                    )
+                }
             }
         }
         .onTapGesture {
